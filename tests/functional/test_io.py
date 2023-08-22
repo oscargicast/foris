ccfba4d287f6ac12b5a523fcfd8eb0911b477d37
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2023-08-22 11:20:27 -0500

    chore: add README

--- tests/functional/test_io.py
+++ tests/functional/test_io.py
@@ -5,8 +5,8 @@ from src.controllers import GenerateReport
 
 def test_reports(io_data_fixture):
     input_file_path, expected_output_file_path = io_data_fixture
-    print(f"Testing... {input_file_path}")
-    print(f"Output... {expected_output_file_path}")
+    print(f"\nTesting : {input_file_path}")
+    print(f"Expected: {expected_output_file_path}")
     output_file_path = GenerateReport(input_file_path).generate()
     with open(output_file_path, "rb") as f1, open(expected_output_file_path, "rb") as f2:
         content1 = f1.read()
