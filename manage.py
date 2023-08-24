a9b477d8fa42ad369f4f8351055820cdac06db25
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2023-08-24 17:01:13 -0500

    feat: enhance manage.py

--- manage.py
+++ manage.py
@@ -1,37 +1,76 @@
+import argparse
 import os
 import pytest
 import sys
 
+from rich import print
 from src.controllers import GenerateReport
 
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 
 def setup_python_path():
     # Add the src directory to the Python path.
     src_dir = os.path.join(PROJECT_ROOT, "src")
     sys.path.insert(0, src_dir)
 
-def get_input_file_path():
-    if len(sys.argv) == 3:
-        arg = sys.argv[2]
-        return arg
-    else:
-        raise FileNotFoundError("A input filename must be specified")
+def test_command(args):
+    print(
+        "\n[bold green]:microbe::microbe: "
+        "Running tests... "
+        ":microbe::microbe:[/bold green]\n"
+    )
+    setup_python_path()
+    pytest.main(args)
+
+def generate_report_command(filename):
+    file_path = filename.filename
+    print(
+        "\n[bold red]:beer::beer: "
+        "Generating report! "
+        ":beer::beer:[/bold red]\n"
+        f"[bold green]Input file:[/bold green] {file_path}\n"
+    )
+    GenerateReport(file_path).generate()
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generates a student attendance report",
+    )
+    subparsers = parser.add_subparsers(title="Commands", dest="command")
+
+    # Subparser for 'test' command.
+    subparsers.add_parser("test", help="Run all tests")
+
+    # Subparser for 'generate_report' command.
+    generate_report_parser = subparsers.add_parser(
+        "generate_report",
+        help="Generate a report",
+    )
+    generate_report_parser.add_argument(
+        "filename",
+        help="Path to the input file to analize",
+    )
+    generate_report_parser.set_defaults(func=generate_report_command)
+
+    # Get the args.
+    args, extra_args = parser.parse_known_args()
+
+    # In case there is no command.
+    if not getattr(args, "command"):
+        print(
+            "No command provided :pensive_face:. "
+            "Use --help to see available commands."
+        )
+        return
+    # In order to handle args with the "-" prefix for the test command.
+    if args.command == "test":
+        test_command(extra_args)
+    # For the other commands, in this case just generate_report.
+    if hasattr(args, "func"):
+        args.func(args)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./manage.py <command>")
-        sys.exit(1)
-
-    command = sys.argv[1]
-    if command == "test":
-        setup_python_path()
-        pytest.main(sys.argv[2:])
-    elif command == "generate_report":
-        file_path = get_input_file_path()
-        GenerateReport(file_path).generate()
-    else:
-        print(f"Unknown command: {command}")
-        sys.exit(1)
+    main()
